dd957ddbd966d61747eb7347d0da06f7bebec301
MajdSoubh <majdsoubh53@gmail.com>
2024-10-07 12:24:17 +0300

    make the default theme to be dark

--- src/components/hooks/useThemeSwitcher.js
+++ src/components/hooks/useThemeSwitcher.js
@@ -1,26 +1,26 @@
 import React, { useEffect, useState } from "react";
 
 const useThemeSwitcher = () => {
-  const [mode, setMode] = useState("");
+  const [mode, setMode] = useState("dark");
 
-  useEffect(() => {
-    const theme = localStorage.getItem("theme");
-    const media = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    if (theme) {
-      if (theme === "dark") {
-        document.documentElement.classList.add("dark");
-        setMode("dark");
-      } else if (theme === "light") {
-        document.documentElement.classList.remove("dark");
-        setMode("light");
-      }
-    } else {
-      const mediaTheme = media ? "dark" : "light";
+  // useEffect(() => {
+  //   const theme = localStorage.getItem("theme");
+  //   const media = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  //   if (theme) {
+  //     if (theme === "dark") {
+  //       document.documentElement.classList.add("dark");
+  //       setMode("dark");
+  //     } else if (theme === "light") {
+  //       document.documentElement.classList.remove("dark");
+  //       setMode("light");
+  //     }
+  //   } else {
+  //     const mediaTheme = media ? "dark" : "light";
 
-      if (mediaTheme === "dark") document.documentElement.classList.add("dark");
-      setMode(mediaTheme);
-    }
-  }, []);
+  //     if (mediaTheme === "dark") document.documentElement.classList.add("dark");
+  //     setMode(mediaTheme);
+  //   }
+  // }, []);
 
   useEffect(() => {
     if (mode == "dark") {
