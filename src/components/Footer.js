ff440a4e20beb5455be0c979d0fe0d4632f5b18a
MajdSoubh <majdsoubh53@gmail.com>
2024-10-07 12:52:20 +0300

    modify footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,8 +12,7 @@ const Footer = () => {
   return (
     <footer className="w-full border-t-2 border-solid border-dark dark:border-white  dark:text-light  font-medium text-lg sm:text-base ">
       <Layout className="!py-10 md:!py-6  flex items-center justify-between lg:flex-col gap-4">
-        <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
-        <Link href="/"> Majd Soubh</Link>
+        <span>Copyright &copy; {new Date().getFullYear()} Majd Soubh</span>
         <Link href="/">
           <div className="flex gap-4">
             <TelegramIcon className="!w-7 !stroke-dark dark:!stroke-light !fill-dark dark:!fill-light" />
